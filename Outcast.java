27e52c06badb8084835d5d69413c0509e64c1bc1
StormKing969 <wijesinghesaj@gmail.com>
2017-04-24 19:02:18 -0400

    Add files via upload

---
+++ Outcast.java
@@ -0,0 +1,42 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+// An immutable data type for outcast detection.
+public class Outcast {
+    private WordNet wordn; 
+    // Construct an Outcast object given a WordNet object.
+    public Outcast(WordNet wordnet) {
+        this.wordn = wordnet; 
+    }
+    // The outcast noun from nouns.
+    public String outcast(String[] nouns) {
+        int max = -1;
+        String outcast = null;
+        // the logic here is to loop through each noun in the array and compare
+        // it to every other noun, computing the sum of their distances.
+        // then check if it is the new max distance.
+        for (int i = 0; i < nouns.length; i++) {
+            int curr_dist = 0;
+            for (int j = 0; j < nouns.length; j++) {
+                //if (i != j) {
+                    curr_dist += wordn.distance(nouns[i], nouns[j]);
+                //}
+            }
+            if (max == -1 || curr_dist > max) {
+                max = curr_dist;
+                outcast = nouns[i];
+            }
+        }
+        return outcast;
+    }
+    // Test client. [DO NOT EDIT]
+    public static void main(String[] args) {
+        WordNet wordnet = new WordNet(args[0], args[1]);
+        Outcast outcast = new Outcast(wordnet);
+        for (int t = 2; t < args.length; t++) {
+            In in = new In(args[t]);
+            String[] nouns = in.readAllStrings();
+            StdOut.println("outcast(" + args[t] + ") = "
+                           + outcast.outcast(nouns));
+        }
+    }
+}
